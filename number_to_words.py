8ab648ce60e9956e23cc32e05af94283233c4a15
Luis Carlos Mejia Garcia <lcmejia19@gmail.com>
2012-01-19 23:09:34 -0600

    correccion en error con numero CIEN

--- number_to_words.py
+++ number_to_words.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 MILES = ['MIL ', 'MILLONES ', 'BILLONES ', 'TRILLONES ', 'CUATRILLONES ', 'QUINTILLONES ', 'SIXTILLONES ', 'SEPTILLONES ', 'OCTILLONES ']
 
 NUMEROS = [
-    ['','CIENTO ', 'DOSCIENTOS ', 'TRESCIENTOS ', 'CUATROCIENTOS ', 'QUINIENTOS ', 'SEISCIENTOS ', 'SETECIENTOS ', 'OCHOCIENTOS ', 'NOVECIENTOS '  ],           
+    ['CIEN ','CIENTO ', 'DOSCIENTOS ', 'TRESCIENTOS ', 'CUATROCIENTOS ', 'QUINIENTOS ', 'SEISCIENTOS ', 'SETECIENTOS ', 'OCHOCIENTOS ', 'NOVECIENTOS '  ],           
     [ '', 'UN ', 'DOS ', 'TRES ', 'CUATRO ', 'CINCO ', 'SEIS ', 'SIETE ', 'OCHO ', 'NUEVE ', 'DIEZ ', 'ONCE ', 'DOCE ', 'TRECE ', 'CATORCE ', 'QUINCE ', 'DIECISEIS ', 'DIECISIETE ', 'DIECIOCHO ', 'DIECINUEVE ', 'VEINTE '],
     ['','DIEZ ','VENTI', 'TREINTA ', 'CUARENTA ', 'CINCUENTA ', 'SESENTA ', 'SETENTA ', 'OCHENTA ', 'NOVENTA ', 'CIEN '],
 ]
@@ -24,34 +24,44 @@ def number_to_words(value,currency= "CORDOBAS"):
     
     words = []
     
-    decimales = "" if int(numbers[1]) == 0 else "CON " + numbers[1] + "/100 "
+    decimals = "" if int(numbers[1]) == 0 else "CON " + numbers[1] + "/100 "
     
     if int(numbers[0])==0:
-        words.append( "CERO ")
-    elif int(numbers[0])==100:
-        words.append( "CIEN ")
+        words.append("CERO ")
+
+    
     else:
         numbers = list(numbers[0])
         n = len(numbers)    
         tens = 0
+        hundreds = 0
         for digit in numbers:
             digit = int(digit)
             pos = n%3
             if pos == 0:
-                words.append(NUMEROS[pos][digit])
+                hundreds = digit
+                if digit !=1:
+                    words.append(NUMEROS[pos][digit])
+                
+                    
             elif pos == 2:
                 tens = digit
                 
             elif pos == 1:
-                if tens >2:
+                if hundreds == 1 :
+                    if tens ==0 and digit == 0:
+                        words.append(NUMEROS[0][0])
+                    else:
+                        words.append(NUMEROS[0][1])
+                elif tens >2:
                     words.append(NUMEROS[2][tens])
                     if digit !=0:
                         words.append("Y ")
                 elif tens ==2:
                     if digit ==0:
-                        words.append("VEINTE ")
+                        words.append(NUMEROS[1][20])
                     else:
-                        words.append("VENTI")
+                        words.append(NUMEROS[2][2])
                 elif tens == 1:
                     
                     words.append(NUMEROS[1][digit + tens*10])
@@ -64,4 +74,4 @@ def number_to_words(value,currency= "CORDOBAS"):
             
             n-=1
     words = "".join(words)    
-    return  words + decimales + currency +  (" NETOS" if decimales =="" else "")
+    return  words + decimals + currency +  (" NETOS" if decimals =="" else "")
